5c02fca98df8619cfe12cd7037796932f41543aa
Sebuuma Daniel <sedzsoft@gmail.com>
2017-11-05 17:46:01 +0300

    Initial commit

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to Andela DevOps Guide\u2019s documentation!"],titleterms:{andela:0,devop:0,document:0,guid:0,indic:0,tabl:0,welcom:0}})
